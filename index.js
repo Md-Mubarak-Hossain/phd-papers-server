da4236f21bb3d450cae8c6bb54d1479c8d092828
Md Mubarak Hossain <hossainmubarak477@gmail.com>
2024-03-02 05:12:27 +0600

    doc added

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+
 const express = require('express');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const cors = require('cors');
@@ -11,7 +12,67 @@ app.use(express.json())
 //     res.send(verify-paper)
 // })
 app.get('/', (req, res) => {
-    res.send(`PHD verify-paper server run`)
+    res.send(`<div style='margin:40px;'>
+    <h2 style='text-align:justify;'>Welcome to the verify paper server...</h2>
+    <div style='text-align: center;'>
+        <h3>This server build up and Presented:-</h3>
+    </div>
+    <div style='display:flex;justify-content:center;'>
+        <ul type='none' style='padding:20px'>
+        <li><u><i style='color:black;'>Submited by:-</i></u></li>
+            <li style=''>Md Mubarak Hossain
+                <ul type='none'>
+                    <li>ID/Roll:191071015</li>
+                    <li>Department of Computer Science and Engineering</li>
+                    <li>Shanto Mariam University Of Creative Technology</li>
+                    <li>Uttara,Dhaka,Bangladesh</li>
+                </ul>
+            </li>
+        </ul>
+        <ul type='none' style='border:2px blue;padding:20px'>
+        <li><u><i style='color:black'>Supervised by:-</i></u></li>
+            <li style=''>Abdullah Bin Masud
+                <ul type='none'>
+                    <li>Coordinator</li>
+                    <li>Department of Computer Science and Engineering</li>
+                    <li>Shanto Mariam University Of Creative Technology</li>
+                    <li>Uttara,Dhaka,Bangladesh</li>
+                </ul>
+            </li>
+        </ul>
+    </div>
+     <div style='text-align: center;'>
+        <h3>This server build up technologies:-</h3>
+    </div>
+    <div style='display:flex;justify-content:center;'>
+    <ul type='none'>
+    <li>Used Technology in this server:-
+       <ul type='square'>
+       <li>NodeJs</li>
+       <li>ExpressJS Framework</li>
+       <li>CorJs middleware</li>
+       <li>DotEnv environment</li>
+       <li>MongoDb Database</li>
+       <li>NodemonJs for auto server runner</li>
+       </ul>
+       </li>
+    </ul>
+    <ul type='none'>
+    <li>Dependencies:-
+    <pre>
+      "dependencies": {
+                "node": "^21.6.2",
+                "express": "^4.18.3",
+                "cors": "^2.8.5",
+                "dotenv": "^16.4.5",
+                "mongodb": "^6.4.0",
+                "nodemon": "^3.1.0"
+                        }
+    </pre>
+    </li>
+    </ul>
+    </div>
+</div>`)
 })
 app.listen(port, () => {
     console.log('PHD PAPER', port)
