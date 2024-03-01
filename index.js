557eea56de1b3754ffc0093ebfe552df189d1ce7
Md Mubarak Hossain <hossainmubarak477@gmail.com>
2024-03-02 05:53:32 +0600

    doc added

--- index.js
+++ index.js
@@ -17,14 +17,15 @@ app.get('/', (req, res) => {
     <title>This verify paper server</title>
     </head>
     <body style='background:lightblue'>
-    <div style='margin:40px;'>
+    
+<div style='margin:40px;'>
     <h2 style='text-align:justify;color:blue'>Welcome to the verify paper server...</h2>
     <div style='text-align: center;'>
         <h3>This server build up and Presented:-</h3>
     </div>
     <div style='display:flex;justify-content:center;'>
         <ul type='none' style='padding:20px'>
-        <li><u><i style='color:black;'>Submited by:-</i></u></li>
+            <li><u><i style='color:black;'>Submited by:-</i></u></li>
             <li style=''>Md Mubarak Hossain
                 <ul type='none'>
                     <li>ID/Roll:191071015</li>
@@ -35,7 +36,7 @@ app.get('/', (req, res) => {
             </li>
         </ul>
         <ul type='none' style='border:2px blue;padding:20px'>
-        <li><u><i style='color:black'>Supervised by:-</i></u></li>
+            <li><u><i style='color:black'>Supervised by:-</i></u></li>
             <li style=''>Abdullah Bin Masud
                 <ul type='none'>
                     <li>Coordinator</li>
@@ -46,54 +47,57 @@ app.get('/', (req, res) => {
             </li>
         </ul>
     </div>
-    
-     <div style='text-align: center;'>
+
+    <div style='text-align: center;'>
         <h3>This server build up technologies:-</h3>
     </div>
     <div style='display:flex;justify-content:center;'>
-    <ul type='none'>
-    <li>Used Technology in this server:-
-       <ul type='square'>
-       <li>NodeJs</li>
-       <li>ExpressJS Framework</li>
-       <li>CorJs middleware</li>
-       <li>DotEnv environment</li>
-       <li>MongoDb Database</li>
-       <li>NodemonJs for auto server runner</li>
-       </ul>
-       </li>
-    </ul>
-    <ul type='none'>
-    <li>Dependencies:-
-    <pre>
-      "dependencies": {
-                "node": "^21.6.2",
-                "express": "^4.18.3",
-                "cors": "^2.8.5",
-                "dotenv": "^16.4.5",
-                "mongodb": "^6.4.0",
-                "nodemon": "^3.1.0"
+        <ul type='none'>
+            <li>Used Technology in this server:-
+                <ul type='square'>
+                    <li>NodeJs</li>
+                    <li>ExpressJS Framework</li>
+                    <li>CorJs middleware</li>
+                    <li>DotEnv environment</li>
+                    <li>MongoDb Database</li>
+                    <li>NodemonJs for auto server runner</li>
+                </ul>
+            </li>
+        </ul>
+        <ul type='none'>
+            <li>Dependencies:-
+                <pre>
+                    "dependencies": {
+                        "node": "^21.6.2",
+                    "express": "^4.18.3",
+                    "cors": "^2.8.5",
+                    "dotenv": "^16.4.5",
+                    "mongodb": "^6.4.0",
+                    "nodemon": "^3.1.0"
                         }
-    </pre>
-    </li>
-    </ul>
-    <ul type='none'>
-    <li>Deployment:-
-                        <dt>Deploy in Vercel</dt>
-                        <dt><a href="https://phd-paper-server.vercel.app/">Server live site</a></dt>
-    </li>
-    </ul>
-    <ul type='none'>
-    <li>Push code:-
-                       <dt>Github</dt>
-                       <dt><a href="https://github.com/Md-Mubarak-Hossain/phd-papers-server">GitHub Source Code</a></dt>
-    </li>
-    </ul>
+                </pre>
+            </li>
+        </ul>
+        <ul type='none'>
+            <li>Deployment:-
+                <dt>Deploy in Vercel</dt>
+                <dt><a href="https://phd-paper-server.vercel.app/">Server live site</a></dt>
+            </li>
+        </ul>
+        <ul type='none'>
+            <li>Push code:-
+                <dt>Github</dt>
+                <dt><a href="https://github.com/Md-Mubarak-Hossain/phd-papers-server">GitHub Source Code</a></dt>
+            </li>
+        </ul>
     </div>
 </div>
+  
     </body>
     </html>`)
 })
+
+
 app.listen(port, () => {
     console.log('PHD PAPER', port)
 })
